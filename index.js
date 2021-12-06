818878e79441a04fdbfa018085dcfa82c94c73fc
Ahmed Ayman <69440618+AhmedAymanBfcai@users.noreply.github.com>
2021-12-06 07:48:17 +0200

    refactor some code

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+process.env.UV_THREADPOOL_SIZE = 1 // This does not the restrict the total number of threads in our entier cluster, So every child in the cluster only has on thread avilable.
 const cluster = require('cluster')
 const express = require('express')
 const app = express()
+const crypto = require('crypto')
 
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
   // Cause index.js to be executed again but in child mode.
   cluster.fork()
 } else {
-  // I am a chile and I will act as a server and nothing else.
-  function doWork(duration) {
-    const start = Date.now()
-    while (Date.now() - start < duration) {}
-  }
+  //   // I am a chile and I will act as a server and nothing else.
+  //   function doWork(duration) {
+  //     const start = Date.now()
+  //     while (Date.now() - start < duration) {}
+  //   }
 
   app.get('/', (req, res) => {
     res.send('Hi there :)')
-    doWork(5000)
+    // doWork(5000)
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there :)')
+    })
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!')
   })
 
   app.listen(3000, () => {
