36c513e81174d8bfd3079fb14215a897532d793b
Ahmed Ayman <69440618+AhmedAymanBfcai@users.noreply.github.com>
2021-12-06 11:01:50 +0200

    trying worker threads technique

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const crypto = require('crypto')
+const Worker = require('webworker-threads').Worker
 
 app.get('/', (req, res) => {
-  res.send('Hi there :)')
-  // doWork(5000)
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there :)')
+  const worker = new Worker(function () {
+    this.onmessage = function () {
+      // This will be invoked will our application calls post message.
+      let counter = 0
+      while (counter < 1e9) {
+        counter++
+      }
+      postMessage(counter)
+    }
   })
+
+  // Whenever our worker sends a message back to our application the function will be called
+  worker.onmessage = function (message) {
+    console.log(message)
+    res.send('' + message.data)
+  }
+
+  worker.postMessage()
 })
 
 app.get('/fast', (req, res) => {
